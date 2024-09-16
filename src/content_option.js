1a34980cc18b212c744cf4de02a92b7c4b574544
Iulian <yuliitezary@hotmail.com>
2024-09-16 10:25:40 +0300

    n
    n

---
+++ src/content_option.js
@@ -0,0 +1,166 @@
+const logotext = "Iulian";
+const meta = {
+    title: "Terentii Iulian",
+    description: "I’m Terentii Iulian data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Terentii Iulian",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "Salut eu ma sunmesc Iulian eu sunt un programator de programe mobile,si web situri ca de exemplu wordpress,html,css,javascript,react u doresct sa ma mai dezvolt in filmarea video si motarea si editarea.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "Tekwill",
+        date: "2023",
+    },
+    {
+        jobtitle: "Tekwill academy kids",
+        where: "prezeent",
+        date: "2023",
+    },
+    {
+        jobtitle: "Coworker",
+        where: "prezent",
+        date: "2024",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Djano",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "cursul trecut",
+    },
+    {
+        title: "Mobile Apps",
+        description: "https://play.google.com/store/apps/dev?id=6663761878144790754",
+    },
+    {
+        title: "Wordpress Design",
+        description: "numai local",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "yuliitezary@hotmail.com",
+    YOUR_FONE: "+373-68289438",
+    description: "Salut eu ma sunmesc Iulian eu sunt un programator de programe mobile,si web situri ca de exemplu wordpress,html,css,javascript,react u doresct sa ma mai dezvolt in filmarea video si motarea si editarea. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_0jcpoq3",
+    YOUR_TEMPLATE_ID: "template_v2h0q18",
+    YOUR_USER_ID: "GW6s4jQMLPF8XL_Ja",
+};
+
+const socialprofils = {
+    github: "https://github.com/yuliitezary",
+    facebook: "https://www.facebook.com/yuliitezaryg/",
+    linkedin: "https://www.linkedin.com/in/terentii-iulian-ba5728275/",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
